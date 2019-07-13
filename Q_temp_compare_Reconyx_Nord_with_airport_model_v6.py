b913d041a88a23c5d061b7874615e52aa8f8f55f
Sophie Dufour-Beauséjour <s.dufour.beausejour@gmail.com>
2019-07-12 20:16:07 -0400

    Made plots more clear

--- Q_temp_compare_Reconyx_Nord_with_airport_model_v6.py
+++ Q_temp_compare_Reconyx_Nord_with_airport_model_v6.py
@@ -152,68 +152,64 @@ intersection = pd.concat([daily_mean["Nord_268"].dropna(), daily_mean["EC"].drop
 intersection.columns = ["Nord_268","EC"]
 intersection["diff"] = intersection["Nord_268"] - intersection["EC"]
 figures_path = "../../figures/meteo/"+script_name[0:-3]+".pdf"
-fig, axes = plt.subplots(figsize=(14, 6)) # (1,1) means one plot, and figsize is w x h in inch of figure
+fig, axes = plt.subplots(3, 3, figsize=(14, 8)) # (1,1) means one plot, and figsize is w x h in inch of figure
 fig.subplots_adjust(left=0.08, right=0.96, bottom=0.1, top=0.92, hspace=0.2, wspace=0.1) # adjust the box of axes regarding the figure size
-gs = gridspec.GridSpec(3, 3,
-                       width_ratios=[1,1,1],
-                       height_ratios=[1,1,1]
-                       )
-axes = list()
-axes.append(plt.subplot(gs[0,0]))
-axes.append(plt.subplot(gs[0,1]))
-axes.append(plt.subplot(gs[0,2]))
-axes.append(plt.subplot(gs[1,0]))
-axes.append(plt.subplot(gs[1,1]))
-axes.append(plt.subplot(gs[1,2]))
-axes.append(plt.subplot(gs[2,0]))
-axes.append(plt.subplot(gs[2,1]))
-axes.append(plt.subplot(gs[2,2]))
-
+axes = axes.flatten()
 fig.suptitle("Comparing Quaqtaq Reconyx Nord Temperature Measurements with Airport", fontsize=12)
 
-smoothed_by_year = {}
 for i, year in enumerate([2015,2016,2017]):
     xmin = mpl.dates.date2num(datetime.datetime(year=int(year),month=int(9),day=int(15)))
     xmax = mpl.dates.date2num(datetime.datetime(year=int(year)+1,month=int(9),day=int(15)))
-    axes[i].set_xlim(xmin=xmin,xmax=xmax)   # limit for xaxis
 
-    # Plot difference
+    # Plot each series
+    axes[i].set_xlim(xmin=xmin,xmax=xmax)   # limit for xaxis
+    axes[i].yaxis.set_ticks(np.arange(-30,30,10))
+    axes[i].yaxis.set_ticks(np.arange(-40,40,2), minor=True)
+    axes[i].set_ylim(-40,30)
+    axes[i].set_ylabel(r"Temperature ($^\circ$C)")
     axes[i].axhline(y=0, ls="-", c="k", linewidth=0.5)
     axes[i].annotate(str(year)+"-"+str(year+1), xy=(0.04,0.875), xycoords="axes fraction",fontsize=12,color="k")
-    axes[i].set_ylabel(r"$\Delta T_{268 - EC}$ ($^\circ$C)")
-    axes[i].yaxis.set_ticks(np.arange(-10,30,1), minor=True)
-    axes[i].plot_date(v_date2num(intersection["diff"].index),intersection["diff"].values, linewidth=0.4 ,marker=" ",linestyle="-",color="k")
-    # Mean diff for that year
-    yearly_mean_diff_ind = (intersection["diff"].index > mpl.dates.num2date(xmin)) & (intersection["diff"].index <= mpl.dates.num2date(xmax))
-    yearly_mean_diff = intersection["diff"][yearly_mean_diff_ind]
-    smooth_diff = savgol_filter(yearly_mean_diff.values, 121, 3) # window size, polynomial order
-    axes[i].plot_date(yearly_mean_diff.index,smooth_diff, linewidth=1 ,marker="",linestyle="-",color="b")
+    axes[i].plot_date(daily_mean["Nord_268"].index, daily_mean["Nord_268"], linewidth=0.4,marker=" ",linestyle="-",color="k")
+    axes[i].plot_date(daily_mean["EC"].index, daily_mean["EC"], linewidth=0.4,marker=" ",linestyle="-",color="r")
+    axes[i].annotate(r"Reconyx", xy=(0.05,0.18), xycoords="axes fraction",fontsize=12,color="k")
+    axes[i].annotate(r"EC", xy=(0.05,0.08), xycoords="axes fraction",fontsize=12,color="r")
 
-    # save with relative date 2015-2016
-    new_index = [datetime.datetime(
-                    year=x.year-i,
-                    month=x.month,
-                    day=x.day) for x in yearly_mean_diff.index]
-    # smoothed_by_year[str(year)] = pd.Series(smooth_diff,new_index)
-    # all_smoothed = pd.concat([smoothed_by_year["2015"],smoothed_by_year["2016"],smoothed_by_year["2017"]],axis=1,join="outer")
-    # mean_smoothed = all_smoothed.mean(axis=1)
-    # mean_smoothed_again = savgol_filter(mean_smoothed.values, 121, 3) # window size, polynomial order
-    new_index = np.asarray(v_date2num(new_index))
-    axes[i+3].plot_date(new_index,smooth_diff, linewidth=1 ,marker="",linestyle="-",color="k")
-    mu = new_index[np.where(smooth_diff == max(smooth_diff))]
-    sigma = 50
-    # print(v_date2num(new_index))
-    gaussian = stats.norm.pdf(new_index, mu, sigma)
-    axes[i+3].plot_date(new_index,4*gaussian/max(gaussian), linewidth=1 ,marker="",linestyle="-",color="r")
-
+    # Correct DB data
     year_data_ind = (intersection["diff"].index > mpl.dates.num2date(xmin)) & (intersection["diff"].index <= mpl.dates.num2date(xmax))
-    year_data = intersection["diff"][year_data_ind]
-    axes[i+6].set_xlim(xmin=xmin,xmax=xmax)   # limit for xaxis
-    axes[i+6].plot_date(v_date2num(year_data.index),year_data.values, linewidth=0.4 ,marker=" ",linestyle="-",color="k")
-    axes[i+6].plot_date(v_date2num(year_data.index),year_data.values - 4*gaussian/max(gaussian), linewidth=0.4 ,marker=" ",linestyle="-",color="r")
-    axes[i+6].axhline(y=0, ls="-", c="k", linewidth=0.5)
+    yearly_diff = intersection["diff"][year_data_ind]
+    smooth_diff = savgol_filter(yearly_diff.values, 121, 3) # window size, polynomial order
+    mu = mpl.dates.date2num(datetime.datetime(year+1,5,1))
+    sigma = 50
+    gaussian = stats.norm.pdf(v_date2num(yearly_diff.index), mu, sigma)
+    bias = -4*gaussian/max(gaussian)
+    axes[i+3].set_xlim(xmin=xmin,xmax=xmax)   # limit for xaxis
+    axes[i+3].plot_date(v_date2num(yearly_diff.index),yearly_diff, linewidth=0.4 ,marker=" ",linestyle="-",color="k")
+    axes[i+3].plot_date(v_date2num(yearly_diff.index),smooth_diff, linewidth=1 ,marker=" ",linestyle="-",color="k")
+    axes[i+3].plot_date(v_date2num(yearly_diff.index),-bias, linewidth=1 ,marker=" ",linestyle="-",color="b")
+    axes[i+3].axhline(y=0, ls="-", c="k", linewidth=0.5)
+    axes[i+3].annotate(r"Reconyx - EC", xy=(0.55,0.18), xycoords="axes fraction",fontsize=12,color="k")
+    axes[i+3].annotate(r"Modeled heating bias", xy=(0.55,0.08), xycoords="axes fraction",fontsize=12,color="b")
+    # axes[3].annotate(r"DB - corrected", xy=(0.65,0.08), xycoords="axes fraction",fontsize=12,color="b")
+    axes[i+3].set_ylabel(r"$\Delta T$ ($^\circ$C)")
+    axes[i+3].yaxis.set_ticks(np.arange(-5,15,5))
+    axes[i+3].yaxis.set_ticks(np.arange(-10,15,1), minor=True)
+    axes[i+3].set_ylim(-6,12)
 
-    # axes[i+3].plot_date(v_date2num(yearly_mean_diff.index),4*gaussian/max(gaussian), linewidth=1 ,marker="",linestyle="-",color="r")
+    # Plot difference
+    yearly_diff_corr = yearly_diff+bias
+    axes[i+6].set_xlim(xmin=xmin,xmax=xmax)   # limit for xaxis
+    axes[i+6].plot_date(v_date2num(yearly_diff_corr.index),yearly_diff_corr, linewidth=0.4 ,marker=" ",linestyle="-",color="k")
+    axes[i+6].axhline(y=0, ls="-", c="k", linewidth=0.5)
+    # Mean diff for that year
+    mean_diff = np.mean(yearly_diff_corr)
+    axes[i+6].axhline(y=mean_diff, ls="--", c="b", linewidth=0.5)
+    axes[i+6].annotate(r"$\Delta T_{mean}$ = "+"{:.1f}".format(mean_diff)+r" $^\circ$C", xy=(0.04,0.85), xycoords="axes fraction",fontsize=12,color="k")
+    axes[i+6].annotate(r"Q$_{corrected}$ - Q$_{EC}$", xy=(0.65,0.08), xycoords="axes fraction",fontsize=12,color="k")
+    axes[i+6].set_ylabel(r"$\Delta T$ ($^\circ$C)")
+    # axes[i+3].yaxis.set_ticks(np.arange(-10,30,1), minor=True)
+    axes[i+6].yaxis.set_ticks(np.arange(-5,15,5))
+    axes[i+6].yaxis.set_ticks(np.arange(-10,15,1), minor=True)
+    axes[i+6].set_ylim(-6,12)
 
 
 # reccurent setup
